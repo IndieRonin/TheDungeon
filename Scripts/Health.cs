cf297e1b5db858563a42dd014731d6ea028f9c71
indieronin <indieronin101@gmail.com>
2021-02-01 05:52:44 +0200

    Created Project

---
+++ Scripts/Health.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public class Health : Node
+{
+    int health;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+}
